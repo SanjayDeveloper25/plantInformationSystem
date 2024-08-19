87909bc5a118e8b1aa9a9c33c7d9827c87c19822
VISHNUVARDHAN25012002 <sanjaydhandapani25@gmail.com>
2024-08-19 13:08:53 +0530

    test

--- pis/src/main/java/com/project/pis/Test.java
+++ pis/src/main/java/com/project/pis/Test.java
@@ -3,5 +3,7 @@ package com.project.pis;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
+
 }
